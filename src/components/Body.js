7683cdf52a07f5064ab5b5015f60aa354ea0126f
DivyaMeghana7686 <divyamegha25@gmail.com>
2023-12-30 04:42:00 +0530

    External_API_DATA

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState  } from "react";
+import { useState,useEffect } from "react";
 import resObjs from "../utils/mockData";
+import { DATA_URL } from "../utils/constants";
 
 const Body=()=>{
     const [listObjs, setListObjs]=useState(resObjs.restaurants);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData= async ()=>{
+        const data= await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json=await data.json();
+        console.log(json);
+        setListObjs(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    };
+    
 
     return (
         <div className="body">
